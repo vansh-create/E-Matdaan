1a776abc296ce6bf859558166e25051939d4e1b8
Nirbhay_Shukla <nirbhayshuklaa@gmail.com>
2026-04-08 14:46:35 +0530

    structrue change of login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, redirect
+from flask import Flask,render_template, redirect,request
 import mysql.connector
 
 app=Flask(__name__)
@@ -7,3 +7,14 @@ app.secret_key='matdan1234'
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login",methods=["POST"])
+def login():
+    return render_template("login.html")
+    if request.method=="POST":
+
+        name=request.form.get("name")
+        voter_id=request.form.get("voter_id")
+        adhar=request.form.get("adhar")
+        password=request.form.get("password")
+    
